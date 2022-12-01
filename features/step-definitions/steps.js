d99311e8671ee8b53245d764ab7f1266b2979af7
ekate <ekaterina89@inbox.lv>
2022-12-01 09:35:26 +0200

    scaffolding of features directory added

---
+++ features/step-definitions/steps.js
@@ -0,0 +1,6 @@
+/*const { Given, When, Then } = require('@wdio/cucumber-framework');
+
+Given("I am in the {string} home page", async (page) => {
+    await browser.url(`https://www.newegg.com/${page}`);
+    await browser.pause(5000);
+});*/
