5bc7390eeeb046def2c59af1d7498f9576a12598
ekate <ekaterina89@inbox.lv>
2022-12-02 20:23:54 +0200

    unneeded code removed

--- features/step-definitions/steps.js
+++
@@ -1,6 +0,0 @@
-// const {Given, When, Then} = require('@wdio/cucumber-framework');
-
-// Given('I am at the home page', async () => {
-//     await browser.url(`https://www.newegg.com`);
-//     await browser.pause(5000);
-// });
